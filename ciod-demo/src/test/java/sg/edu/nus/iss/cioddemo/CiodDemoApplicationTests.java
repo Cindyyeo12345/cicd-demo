769d1d7b461448635b1a5a37c6f5d7ff120ee246
Cindy <e1336362@u.nus.edu>
2024-01-20 15:00:54 +0800

    initial push

---
+++ ciod-demo/src/test/java/sg/edu/nus/iss/cioddemo/CiodDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.cioddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiodDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
